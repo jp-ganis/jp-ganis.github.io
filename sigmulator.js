e885aefab2de13183cc4b5049708b884545febad
Jp Ganis <jp@tichondrius.home>
2017-04-20 22:36:39 +0100

    First steps, working curve.

--- sigmulator.js
+++ sigmulator.js
@@ -1,7 +1,7 @@
 function d3() { return Math.floor(Math.random() * 3)+1 }
 function d6() { return Math.floor(Math.random() * 6)+1 }
 
-var Attack = {
+let Attack = {
 	attacks: 0,
 	toHit: 0,
 	toWound: 0,
@@ -39,36 +39,36 @@ var Attack = {
 	
 	// checked rolls
 	rollToHit: function() {
-		var h = this.hitRoll();
+		let h = this.hitRoll();
 		if (h >= this.toHit) return 1;
 		else if (this.hitRerolls.indexOf(h) >= 0 && d6() >= this.toHit) return 1;
 		return 0;
 	},
 	rollToWound: function() {
-		var w = this.woundRoll();
+		let w = this.woundRoll();
 		if (w >= this.toWound) return 1;
 		else if (this.woundRerolls.indexOf(w) >= 0 && d6() >= this.toWound) return 1;
 		return 0;
 	},
 	rollToSave: function() {
-		var s = this.saveRoll();
-		if (s >= this.toSave) return 1;
+		let s = this.saveRoll();
+		if (s >= this.toSave + Math.abs(this.rend)) return 1;
 		else if (this.saveRerolls.indexOf(s) >= 0 && d6() >= this.Save) return 1;
 		return 0;
 	},
 	rollDamage: function() {
-		var d = this.damageRoll() + this.damageProc;
+		let d = this.damageRoll() + this.damageProc;
 		this.damageProc = 0;
 		return d;
 	},
 
 	// resolve
 	resolve: function() {
-		for (var a = 0; a < this.attacks; a++) this.hits += this.rollToHit();
-		for (var h = 0; h < this.hits; h++) this.wounds += this.rollToWound();
-		for (var w = 0; w < this.wounds; w++) this.saves += this.rollToSave();
-		for (var s = 0; s < this.saves; s++) this.damageDone += this.rollDamage();
-		var retVal = this.damageDone;
+		for (let a = 0; a < this.attacks; a++) this.hits += this.rollToHit();
+		for (let h = 0; h < this.hits; h++) this.wounds += this.rollToWound();
+		for (let w = 0; w < this.wounds; w++) this.saves += this.rollToSave();
+		for (let s = 0; s < this.saves; s++) this.damageDone += this.rollDamage();
+		let retVal = this.damageDone;
 		this.hits = 0;
 		this.wounds = 0;
 		this.saves = 0;
@@ -80,16 +80,16 @@ var Attack = {
 
 // at least function
 function atLeast(attack) {
-	var iterations=5000;
+	let iterations=15000;
 	d = [];
 	e = [];
-	for (var a = 0; a < 500; a++) d[a] = 0;
-	for (var i = 0; i < iterations; i++)
+	for (let a = 0; a < 500; a++) d[a] = 0;
+	for (let i = 0; i < iterations; i++)
 	{
 		dmg = attack.resolve();
-		for (var j = 0; j < dmg; j++) d[j]++;
+		for (let j = 0; j < dmg; j++) d[j]++;
 	}
-	for (var i = 0; i < d.length; i++)
+	for (let i = 0; i < d.length; i++)
 	{
 		d[i] = Math.floor(d[i]/iterations*100);
 		if (d[i] == 0) continue;
@@ -98,19 +98,20 @@ function atLeast(attack) {
 	return e;
 }
 function pad(num, size) {
-	var s = num+"";
+	let s = num+"";
 	while (s.length < size) s = "0" + s;
 	return s;
 }
 
 // fancyPrint
 function fancyPrint(d) {
-	var retString = "";
-	for (var i = 0; i < d.length; i++)
+	let retString = "";
+	for (let i = 0; i < d.length; i++)
 	{
-		var damage = pad(i, 3);
-		var lines = Array(d[i]+1).join("|");
-		var percent = d[i].toString();
+		if (d[i] == 100) continue;
+		let damage = pad(i, 2);
+		let lines = Array(d[i]+1).join("|");
+		let percent = d[i].toString();
 		retString = retString.concat(damage);
 		retString = retString.concat(" ");
 		retString = retString.concat(lines);
@@ -132,5 +133,3 @@ function fancyResolve(a, h, w, r, d, s)
 	Attack.toSave = s;
 	return fancyPrint(atLeast(Attack));
 }
-
-
