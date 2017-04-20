3d7b10332ff5c258959ef5df3e31199d5d862be3
Jp Ganis <jp@tichondrius.home>
2017-04-20 21:38:33 +0100

    fixed bugs in initial commit

--- sigmulator.js
+++ sigmulator.js
@@ -21,45 +21,45 @@ var Attack = {
 	saveModifier: 0,
 	
 	//basic rolls
-	hitRoll: function () { return d6() + this.hitModifier },
-	woundRoll: function () { return d6() + this.woundModifier },
-	saveRoll: function () { return d6() + this.saveModifier },
+	hitRoll: function () { return d6() + this.hitModifier; },
+	woundRoll: function () { return d6() + this.woundModifier; },
+	saveRoll: function () { return d6() + this.saveModifier; },
 	damageRoll: function () {
 		switch(this.damage) {
 			case 8:
-				return d6()
-				break
+				return d6();
+				break;
 			case 7:
-				return d3()
-				break
+				return d3();
+				break;
 			default:
-				return this.damage
+				return this.damage;
 		}
 	},
 	
 	// checked rolls
 	rollToHit: function() {
-		var h = this.hitRoll()
+		var h = this.hitRoll();
 		if (h >= this.toHit) return 1;
 		else if (this.hitRerolls.indexOf(h) >= 0 && d6() >= this.toHit) return 1;
 		return 0;
 	},
 	rollToWound: function() {
-		var w = this.woundRoll()
+		var w = this.woundRoll();
 		if (w >= this.toWound) return 1;
 		else if (this.woundRerolls.indexOf(w) >= 0 && d6() >= this.toWound) return 1;
 		return 0;
 	},
 	rollToSave: function() {
-		var s = this.saveRoll()
+		var s = this.saveRoll();
 		if (s >= this.toSave) return 1;
 		else if (this.saveRerolls.indexOf(s) >= 0 && d6() >= this.Save) return 1;
 		return 0;
 	},
 	rollDamage: function() {
-		var d = this.damageRoll() + this.damageProc
-		this.damageProc = 0
-		return d
+		var d = this.damageRoll() + this.damageProc;
+		this.damageProc = 0;
+		return d;
 	},
 
 	// resolve
@@ -68,34 +68,34 @@ var Attack = {
 		for (var h = 0; h < this.hits; h++) this.wounds += this.rollToWound();
 		for (var w = 0; w < this.wounds; w++) this.saves += this.rollToSave();
 		for (var s = 0; s < this.saves; s++) this.damageDone += this.rollDamage();
-		var retVal = this.damageDone
-		this.hits = 0
-		this.wounds = 0
-		this.saves = 0
-		this.damageDone = 0
-		return retVal
+		var retVal = this.damageDone;
+		this.hits = 0;
+		this.wounds = 0;
+		this.saves = 0;
+		this.damageDone = 0;
+		return retVal;
 	},
 
 }
 
 // at least function
 function atLeast(attack) {
-	var iterations=5000
-	d = []
-	e = []
+	var iterations=5000;
+	d = [];
+	e = [];
 	for (var a = 0; a < 500; a++) d[a] = 0;
 	for (var i = 0; i < iterations; i++)
 	{
-		dmg = attack.resolve()
+		dmg = attack.resolve();
 		for (var j = 0; j < dmg; j++) d[j]++;
 	}
 	for (var i = 0; i < d.length; i++)
 	{
 		d[i] = Math.floor(d[i]/iterations*100);
 		if (d[i] == 0) continue;
-		e[i] = d[i]
+		e[i] = d[i];
 	}
-	return e
+	return e;
 }
 function pad(num, size) {
 	var s = num+"";
@@ -105,32 +105,32 @@ function pad(num, size) {
 
 // fancyPrint
 function fancyPrint(d) {
-	var retString = ""
+	var retString = "";
 	for (var i = 0; i < d.length; i++)
 	{
-		var damage = pad(i, 3)
-		var lines = Array(d[i]+1).join("|")
-		var percent = d[i].toString()
-		retString = retString.concat(damage)
-		retString = retString.concat(" ")
-		retString = retString.concat(lines)
-		retString = retString.concat(" ")
-		retString = retString.concat(percent)
-		retString = retString.concat("%")
-		retString = retString.concat("</br>")
+		var damage = pad(i, 3);
+		var lines = Array(d[i]+1).join("|");
+		var percent = d[i].toString();
+		retString = retString.concat(damage);
+		retString = retString.concat(" ");
+		retString = retString.concat(lines);
+		retString = retString.concat(" ");
+		retString = retString.concat(percent);
+		retString = retString.concat("%");
+		retString = retString.concat("\n");
 	}
-	return retString
+	return retString;
 }
 
-function fancyResolve(x)
+function fancyResolve(a, h, w, r, d, s)
 {
-	Attack.damage = x
-	Attack.attacks = 50
-	Attack.toHit = 4
-	Attack.toWound = 4
-	Attack.toSave = 4
-	return atLeast(Attack)
-	return fancyPrint(atLeast(Attack))
+	Attack.attacks = a;
+	Attack.toHit = h;
+	Attack.toWound = w;
+	Attack.rend = r;
+	Attack.damage = d;
+	Attack.toSave = s;
+	return fancyPrint(atLeast(Attack));
 }
 
 
